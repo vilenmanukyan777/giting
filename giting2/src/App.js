805bbc6e6eb74cd40f5f346e07f8cd07e0255bf4
vilenmanukyan777 <89537281+vilenmanukyan777@users.noreply.github.com>
2024-07-04 14:14:52 +0400

    second commit

--- giting2/src/App.js
+++ giting2/src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react"
+
 const App = () => {
+  const [isValid, setIsValid] = useState(false);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+      name === 'Narek' ? setName('Vilen') : setName('Narek')
+  },[isValid])
+  
+
+  const changeIsValid = () => setIsValid(!isValid)
 
 
   return (
     <section>
-      
+         <p> {name} </p>
+         <button onClick={changeIsValid}> changeName </button>
     </section>
   )
 }
